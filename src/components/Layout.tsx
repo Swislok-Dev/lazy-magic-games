9d23ba3be4ed4f312112216e8e35cda232c89cb6
cm@MBA <cmarker4@gmail.com>
2023-03-08 20:23:27 -0600

    change favicon

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -14,7 +14,7 @@ const Layout: React.FC<Props> = ({ children }) => {
         <title>Lazy Magic Games</title>
         <meta name="description" content="Lazy Magic Games website" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/images/favicon.png" />
       </Head>
       <div className="main">
         <header>
