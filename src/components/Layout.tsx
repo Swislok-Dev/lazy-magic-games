14aa7a403cab5964ec62f126c566108ca5b075f6
cm@MBA <cmarker4@gmail.com>
2023-03-16 11:35:05 -0500

    fix: react-icons/ti import

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
-import { TiSocialFacebook, TiSocialYoutube } from 'react-icons/Ti';
+import { TiSocialFacebook, TiSocialYoutube } from 'react-icons/ti';
 
 type Props = {
   children: any;
