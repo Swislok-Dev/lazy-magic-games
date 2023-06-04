0def3a481f8395828ee8c8ecb7cca4ee5d17fbab
cm@MBA <cmarker4@gmail.com>
2023-06-04 13:27:36 -0600

    update home page

---
+++ src/components/WhatWeHaveDone.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import Image from 'next/image';
+
+function WhatWeHaveDone() {
+  return (
+    <>
+      <Image
+        src={'/images/What_we_have_done/LMG_presents.png'}
+        height={1200}
+        width={1200}
+        alt={'LMG presents'}
+      />
+      <h2 className="text-centered uppercase bg-black-dark">
+        What we have done
+      </h2>
+      <Image
+        src={'/images/What_we_have_done/what_we_did.png'}
+        height={1200}
+        width={1200}
+        alt={'LMG presents'}
+      />
+      <h2 className="text-centered uppercase bg-black-dark">
+        What we are doing
+      </h2>
+      <Image
+        src={'/images/What_we_have_done/eventpics2.png'}
+        height={1200}
+        width={1200}
+        alt={'LMG presents'}
+      />
+      <span className="bg-black-dark text-centered"></span>
+      <Image
+        src={'/images/What_we_have_done/eventpics.png'}
+        height={1200}
+        width={1200}
+        alt={'LMG presents'}
+      />
+      <h2 className="text-centered uppercase bg-black-dark">
+        Where we are going
+      </h2>
+      <Image
+        src={'/images/What_we_have_done/where_we_are_going.png'}
+        height={1200}
+        width={1200}
+        alt={'LMG presents'}
+      />
+    </>
+  );
+}
+
+export default WhatWeHaveDone;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,21 @@
 import Layout from '../components/Layout';
 import Image from 'next/image';
 import Link from 'next/link';
+import WhatWeHaveDone from '../components/WhatWeHaveDone';
 
 export default function Home() {
   return (
     <Layout>
+      <section>
+        {/* <p className="text-overlay bg-black-dark uppercase">more to come!</p> */}
+        <WhatWeHaveDone />
+      </section>
       <section id="page-hero">
         <div id="page-hero-text">
           <h2>Join our gaming community</h2>
           <p>
-            Live streams, discussions, co-operative, competitive board, and
-            tabletop games. What are you waiting for?
+            Live streams, discussions, co-operative, competitive
+            board, and tabletop games. What are you waiting for?
           </p>
           <Link
             href="https://discord.com/invite/6kaKFBYtBZ"
@@ -33,8 +38,8 @@ export default function Home() {
         <div className="text-overlay">
           <h2>It&apos;s here!!</h2>
           <p>
-            The big day may have come and gone, but here is the link if you
-            missed it.
+            The big day may have come and gone, but here is the link
+            if you missed it.
           </p>
           <Link
             href="https://www.thegamecrafter.com/games/project-titan:-strategy-card-game"
@@ -52,35 +57,29 @@ export default function Home() {
           className="image"
         />
       </section>
-      <section>
-        <p className="text-overlay bg-black-dark uppercase">more to come!</p>
-      </section>
+
       <section>
         <Image
           src="/images/PT_defectors_edition_cropped_and_logo.png"
-          width={500}
-          height={500}
+          width={1200}
+          height={1200}
           alt="Project Titan Defectors Edition Banner"
           className="full-image"
         />
         <div className=" bottom">
           <h2 className="uppercase">Unite</h2>
-          <Image 
-          src="/images/Downward_Sword_Icon.png"
-
-          width={200}
-          height={200}
-          alt="Project Titan iconography"
-          
+          <Image
+            src="/images/Downward_Sword_Icon.png"
+            width={200}
+            height={200}
+            alt="Project Titan iconography"
           />
           <h2 className="uppercase">Explore</h2>
-          <Image 
-          src="/images/Downward_Sword_Icon.png"
-
-          width={200}
-          height={200}
-          alt="Project Titan iconography"
-          
+          <Image
+            src="/images/Downward_Sword_Icon.png"
+            width={200}
+            height={200}
+            alt="Project Titan iconography"
           />
           <h2 className="uppercase">Dominate</h2>
         </div>
@@ -90,33 +89,38 @@ export default function Home() {
           <div className="about-lmg">
             <h2 className="uppercase">All kinds of games</h2>
             <p>
-              Lazy Magic Games is dedicated to bringing a uniqure and fun
-              experience to every game we product! Our later piece of work is
-              non-other than <strong>Project Titan: Strategy Card Game.</strong>{' '}
-              Though PTSCG is a card game dedicated to combat, we at LMG have a
-              litany of games being worked on for the consumer. All of which are
-              unique and provide a new experience with each playthrough.
+              Lazy Magic Games is dedicated to bringing a uniqure and
+              fun experience to every game we product! Our later piece
+              of work is non-other than{' '}
+              <strong>Project Titan: Strategy Card Game.</strong>{' '}
+              Though PTSCG is a card game dedicated to combat, we at
+              LMG have a litany of games being worked on for the
+              consumer. All of which are unique and provide a new
+              experience with each playthrough.
             </p>
           </div>
           <div className="about-lmg">
             <h2 className="uppercase">Bringing the game to you</h2>
             <p>
-              As creators we find ourselves wanting to help the customer, in any
-              way we can, to understand rules, artwork, or why we do what we do.
-              That being said we provide many video instructions on all of our
-              games. As well as open lines of communication to discuss, amongst
-              the community, what issues they have or rules in question.
+              As creators we find ourselves wanting to help the
+              customer, in any way we can, to understand rules,
+              artwork, or why we do what we do. That being said we
+              provide many video instructions on all of our games. As
+              well as open lines of communication to discuss, amongst
+              the community, what issues they have or rules in
+              question.
             </p>
           </div>
           <div className="about-lmg">
             <h2 className="uppercase">About Me</h2>
             <p>
-              I grew up playing games with my father who was ultimately the
-              reason why I started to develop games on my own. My father was a
-              man of great imagination and whimsicalness. It was he who
-              instilled in me the foundation I use in all our games. That being,
-              &quot;Chase everything <strong>you</strong> are passionate about
-              because no one else will.&quot;
+              I grew up playing games with my father who was
+              ultimately the reason why I started to develop games on
+              my own. My father was a man of great imagination and
+              whimsicalness. It was he who instilled in me the
+              foundation I use in all our games. That being,
+              &quot;Chase everything <strong>you</strong> are
+              passionate about because no one else will.&quot;
             </p>
           </div>
         </article>
