70036507c072fe688a14612a6c2b7c4fffd36657
Swislok-Dev <cmarker4@gmail.com>
2025-04-14 23:47:41 -0500

    format to double quotes

--- src/components/partials/WhatWeHaveDone.tsx
+++ src/components/partials/WhatWeHaveDone.tsx
@@ -1,50 +1,50 @@
-import React from 'react';
-import Image from 'next/image';
+import React from "react";
+import Image from "next/image";
 
 function WhatWeHaveDone() {
   return (
     <>
       <Image
-        src={'/images/What_we_have_done/LMG_presents.png'}
+        src={"/images/What_we_have_done/LMG_presents.png"}
         height={1200}
         width={1200}
-        alt={'LMG presents'}
+        alt={"LMG presents"}
       />
       <h2 className="text-centered uppercase bg-black-dark">
         What we have done
       </h2>
       <Image
-        src={'/images/What_we_have_done/what_we_did.png'}
+        src={"/images/What_we_have_done/what_we_did.png"}
         height={1200}
         width={1200}
-        alt={'LMG presents'}
+        alt={"LMG presents"}
       />
       <h2 className="text-centered uppercase bg-black-dark">
         What we are doing
       </h2>
       <Image
-        src={'/images/What_we_have_done/eventpics2.png'}
+        src={"/images/What_we_have_done/eventpics2.png"}
         height={1200}
         width={1200}
-        alt={'LMG presents'}
+        alt={"LMG presents"}
         id="eventpics2"
       />
       <span className="bg-black-dark text-centered"></span>
       <Image
-        src={'/images/What_we_have_done/eventpics.png'}
+        src={"/images/What_we_have_done/eventpics.png"}
         height={1200}
         width={1200}
-        alt={'LMG presents'}
+        alt={"LMG presents"}
         id="eventpics"
       />
       <h2 className="text-centered uppercase bg-black-dark">
         Where we are going
       </h2>
       <Image
-        src={'/images/What_we_have_done/where_we_are_going.png'}
+        src={"/images/What_we_have_done/where_we_are_going.png"}
         height={1200}
         width={1200}
-        alt={'LMG presents'}
+        alt={"LMG presents"}
       />
     </>
   );
